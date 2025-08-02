ab34c7b1b34f82a91983a30b37fe598677b514aa
jodian kennedy <jodiankennedy@yahoo.com>
2025-08-02 16:37:23 -0400

    Logged student's name and age

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ let student = {
     age: 33,
     enrolled: true,
     courses: "math, english,chemistry,biology",
+    greet: function() {
+        console.log("Hello, my name is " + this.name + " I am " + this.age + "years old and I am enrolled in the following courses: " + this.courses);
+        return true;
+    }
+
 };
+
+console.log(student.name);
+console.log(student.age);
