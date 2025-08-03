4a23f6e5271eabbf10b2b6d7325793c2cb1dd9e8
jodian kennedy <jodiankennedy@yahoo.com>
2025-08-02 20:54:26 -0400

    Converted JSON string back into javascript

--- script.js
+++ script.js
@@ -22,3 +22,6 @@ student.greet();
 
 let jsonString = '{"name":"Kadi","age":33,"enrolled":true,"courses":["Math","English","Chemistry","Biology"]}';
 console.log(jsonString);
+let jsonObj = JSON.parse(jsonString);
+console.log(jsonObj.name, jsonObj.age, jsonObj.enrolled); 
+jsonObj.courses.forEach(course => console.log(course));
